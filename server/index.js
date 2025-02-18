4d6520acf82c5efa697750874a2819ea72525977
Sai472002 <saidhasun0407@gmail.com>
2025-02-18 17:12:41 +0530

    fix api error

--- server/index.js
+++ server/index.js
@@ -16,10 +16,11 @@ app.use(cors({
     origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true, 
+    allowedHeaders: ['Content-Type', 'Authorization']
   }));
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
+  app.use(express.json({ limit: '200mb' }));  
+  app.use(express.urlencoded({ limit: '200mb', extended: true }));
 Connection()
 
 app.get("/",(req,res)=>{
