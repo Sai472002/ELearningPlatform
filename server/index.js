fe6c17312aa045a6b240bf96fe3369eb79f0286c
Sai472002 <saidhasun0407@gmail.com>
2025-02-18 21:02:25 +0530

    fix

--- server/index.js
+++ server/index.js
@@ -19,6 +19,8 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization']
   }));
 
+
+
   app.use(express.json({ limit: '200mb' }));  
   app.use(express.urlencoded({ limit: '200mb', extended: true }));
 Connection()
